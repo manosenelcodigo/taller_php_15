a17ed8d424dc16799d63eb64fd113d4373978ade
#manosenelcódigo <peligrosamente@gmail.com>
2016-07-12 22:05:52 -0400

    Create ajax_1.php

---
+++ ajax_1.php
@@ -0,0 +1,12 @@
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Ventana con ajax</h4>
+      </div>
+      <div class="modal-body">
+        <h1> Parámetro : <?php echo $_POST["id"];?> </h1>
+      </div>
+      
+    </div>
+  </div>
